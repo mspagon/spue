484b53e9a4bc9c6e53bc9a0a10b94d5043859564
Michael Spagon <mspagon@gmail.com>
2019-08-06 04:50:49 -0500

    Adds ability to control light state.

---
+++ spue/state.py
@@ -0,0 +1,28 @@
+import json
+import os
+import requests
+
+
+def on(ip, user, light):
+    body = {"on": True}
+    response = requests.put(
+        "{ip}/api/{user}/lights/{light}/state".format(ip=ip, user=user, light=light),
+        data=json.dumps(body)
+    )
+    return response.json()
+
+
+def off(ip, user, light):
+    body = {"on": False}
+    response = requests.put(
+        "{ip}/api/{user}/lights/{light}/state".format(ip=ip, user=user, light=light),
+        data=json.dumps(body)
+    )
+    return response.json()
+
+
+if __name__ == '__main__':
+    ip = os.getenv("HUE_IPADDRESS")
+    user = os.getenv("HUE_USER")
+    response = on(ip, user, 5)
+    print(response)
